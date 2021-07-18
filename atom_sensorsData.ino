1897e846cfde10140bc30a739e638cce384ce3b4
mixlaab <mixlaab.labs@gmail.com>
2021-07-18 16:15:33 -0500

    Read temperature sensor data

--- atom_sensorsData.ino
+++ atom_sensorsData.ino
@@ -16,11 +16,13 @@
 //MPU-6050 returns values in 16-bit integers
 //Raw values
 int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
+int16_t tempRaw;
 
 //Accelerometer, gyroscope and angles arrays
 float Acc[2];
 float Gy[3];
 float Angle[3];
+float temp;
 
 String values;
 
@@ -51,47 +53,51 @@ void setup()
 void loop()
 {
   // Update IMU sensor values
-  //Leer los valores del Acelerometro de la IMU
+  //Read accelerometer values
    Wire.beginTransmission(MPU6050);
-   Wire.write(0x3B); //Pedir el registro 0x3B - corresponde al AcX
+   Wire.write(0x3B); //Register 0x3B - AcX
    Wire.endTransmission(false);
-   Wire.requestFrom(MPU6050,6,true);   //A partir del 0x3B, se piden 6 registros
-   AcX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
+   Wire.requestFrom(MPU6050,6,true);   //From 0x3B, request 6 registers
+   AcX=Wire.read()<<8|Wire.read(); //Each value occupies 2 registers
    AcY=Wire.read()<<8|Wire.read();
    AcZ=Wire.read()<<8|Wire.read();
  
-   //A partir de los valores del acelerometro, se calculan los angulos Y, X
-   //respectivamente, con la formula de la tangente.
+   //With accelerometer values, calculate angles
    Acc[1] = atan(-1*(AcX/A_R)/sqrt(pow((AcY/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
    Acc[0] = atan((AcY/A_R)/sqrt(pow((AcX/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
  
-   //Leer los valores del Giroscopio
+   //Read gyroscope values
    Wire.beginTransmission(MPU6050);
    Wire.write(0x43);
    Wire.endTransmission(false);
-   Wire.requestFrom(MPU6050,6,true);   //A partir del 0x43, se piden 6 registros
-   GyX=Wire.read()<<8|Wire.read(); //Cada valor ocupa 2 registros
+   Wire.requestFrom(MPU6050,6,true);   //From 0x43, request 6 registers
+   GyX=Wire.read()<<8|Wire.read(); //Each value occupies 2 registers
    GyY=Wire.read()<<8|Wire.read();
    GyZ=Wire.read()<<8|Wire.read();
  
-   //Calculo del angulo del Giroscopio
+   //Get gyroscoper angle
    Gy[0] = GyX/G_R;
    Gy[1] = GyY/G_R;
    Gy[2] = GyZ/G_R;
 
-   dt = (millis() - prev_time) / 1000.0;
-   prev_time = millis();
- 
-   //Aplicar el Filtro Complementario
-   Angle[0] = 0.98 *(Angle[0]+Gy[0]*dt) + 0.02*Acc[0];
-   Angle[1] = 0.98 *(Angle[1]+Gy[1]*dt) + 0.02*Acc[1];
+  //Read temperature sensor values
+  Wire.beginTransmission(MPU6050);                 
+  Wire.write(0x41);      //pointing Temp_Out_High Reg
+  Wire.endTransmission(false); 
+  
+  Wire.requestFrom(MPU6050, 2); //two-byte temp data from Temp_H and Temp_L
+  tempRaw = Wire.read()<<8|Wire.read(); 
+  temp = (float)((tempRaw/340.0) + 36.53);  //formula from data sheets
 
-   //Integración respecto del tiempo paras calcular el YAW
-   Angle[2] = Angle[2]+Gy[2]*dt;
+  dt = (millis() - prev_time) / 1000.0;
+  prev_time = millis();
  
-   //Mostrar los valores por consola
-   //values = "90, " +String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]) + ", -90";
-   //Serial.println(values);
+  //Apply Complementary Filter
+  Angle[0] = 0.98 *(Angle[0]+Gy[0]*dt) + 0.02*Acc[0];
+  Angle[1] = 0.98 *(Angle[1]+Gy[1]*dt) + 0.02*Acc[1];
+
+  //Integration to get Yaw value
+  Angle[2] = Angle[2]+Gy[2]*dt;
    
   webServer.handleClient();
   delay(10);
@@ -110,6 +116,10 @@ void handleRoot()
   response += "\"illuminance\":[{";
   response += "\"value\":";
   response += value;
+  response += "}],";
+  response += "\"temperature\":[{";
+  response += "\"celsius\":";
+  response += temp;
   response += "}],";
   response += "\"angles\":[{";
   response += "\"roll\":";
@@ -153,19 +163,3 @@ void setupWebServer()
   webServer.begin();
   Serial.println("[WebServer] Running!");
 }
-
-/*
-void setupSensorMPU6050(){
-  Serial.println("[MPU6050] Setup");
-  Serial.print("[MPU6050] Connecting..");
-
-  while (!mpu6050.begin())
-  {
-    delay(200);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.println("[MPU6050] Connected!");
-}
-*/
